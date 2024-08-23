d9896bb9c9547d60fff2eb92dccc1916c9da38bb
0Turtlez <misterzd@icloud.com>
2024-08-22 20:32:07 -0500

    Initial Commit

---
+++ apikey.py
@@ -0,0 +1,2 @@
+def get_api_key():
+    return '$2a$10$GQOWFGou.LYomqqhzth/Qegb0KY8p9HZ/m6gdT5Vp5rROwJEAdPRa'
---
+++ visualcontrol.py
@@ -0,0 +1,114 @@
+import csv
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QScrollArea, QFrame
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Intialize Window Settings
+        self.init_ui()
+
+    def init_ui(self):
+        ''' Intialize the main UI settings'''
+        self.setWindowTitle('Shell Forge')
+        self.resize(2000, 1500)
+        self.center_window()
+
+
+        # Create Scroll area and loadcontent
+        self.create_scroll_area()
+        self.populate_mod_data()
+
+    def create_scroll_area(self):
+        ''' Create the scroll area '''
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        self.layout = QVBoxLayout(central_widget)        # Create a layout for the central widget
+
+        # Scroll Area and content
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_content = QFrame()
+        self.scroll_layout = QVBoxLayout(self.scroll_content)
+        self.scroll_area.setWidget(self.scroll_content)
+        #  self.scroll_content.setLayout(self.scroll_layout)
+
+        # Add scroll area to the main layout
+        self.layout.addWidget(self.scroll_area)
+
+    def populate_mod_data(self):
+        with open('data/mainmenu.csv', 'r', encoding='utf-8') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                name = row['Mod_Name']
+                description = row['Description']
+                thumbnail = row['Thumbnail_URL']
+                webpage_URL = row['WebPage_URL']
+                authors = row['Author_Name']
+                downloads = row['Downloads']
+
+
+                self.add_mod_info(
+                    name,
+                    webpage_URL,
+                    description,
+                    thumbnail,
+                    authors,
+                    downloads,
+                )
+                print(row)
+
+    def add_mod_info(self, name, link, description, thumbnail, authors, downloads):
+        labels = [
+            QLabel(f"<a href='{thumbnail}'>Mod Thumbnail Link"),
+            QLabel(f"Mod Name: {name}"),
+            QLabel(f"Mod Description: {description}"),
+            QLabel(f"<a href='{link}'>Mod Link"),
+            QLabel(f"Mod Author(s): {authors}"),
+            QLabel(f"Mod Download Count: {downloads}\n\n"),
+        ]
+        for label in labels:
+            # Set the text format to allow for rich text (HTML)
+            label.setTextFormat(Qt.RichText)
+            # Allow links to be clickable and open in a web browser
+            label.setOpenExternalLinks(True)
+            # Add the label to the layout
+            self.scroll_layout.addWidget(label)
+
+    def center_window(self):
+        # Get the geometry of the window
+        window_geometry = self.frameGeometry()
+
+        # Get the screen resolution
+        screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
+        screen_geometry = QApplication.desktop().screenGeometry(screen)
+
+        # Calculate the center point
+        center_point = screen_geometry.center()
+
+        # Move the window to the center point
+        window_geometry.moveCenter(center_point)
+        self.move(window_geometry.topLeft())
+        # Replace this with reading the csv
+
+
+
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
+
+
+
+'''
+TODO:
+- Get images to visualize on windo 
+- Favoritess
+
+
+'''
